70f7f3c4353acb248c2be86a562bf9026b044dfa
Andrew Ding <afding@gmail.com>
2015-09-01 20:20:58 -0500

    Progress 8/31
    All work done on 8/31 for HF C#

---
+++ Chapter9/StreamWriterMagnets/Flobbo.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace StreamWriterMagnets
+{
+    class Flobbo
+    {
+        private string zap;
+
+        public Flobbo(string zap)
+        {
+            this.zap = zap;
+        }
+
+        public StreamWriter Snobbo()
+        {
+            return new StreamWriter(@"macaw.txt");
+        }
+
+        public bool Blobbo(StreamWriter sw)
+        {
+            sw.WriteLine(zap);
+            zap = "green purple";
+            return false;
+        }
+
+        public bool Blobbo(bool already, StreamWriter sw)
+        {
+            if (already)
+            {
+                sw.WriteLine(zap);
+                sw.Close();
+                return false;
+            }
+            else
+            {
+                sw.WriteLine(zap);
+                zap = "red orange";
+                return true;
+            }
+        }
+    }
+}
